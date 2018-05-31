33753f0a257bea07107791c06ab2027da24fc478
ohager <oliver.hager@dextra-sw.com>
2018-05-31 11:40:23 -0300

    organized funcs in class

--- src/keyListener.js
+++ src/keyListener.js
@@ -24,4 +24,4 @@ class KeyListener {
 	
 }
 
-module.exports = new KeyListener();
+module.exports = KeyListener;
--- src/watchdog.js
+++ src/watchdog.js
@@ -1,89 +1,102 @@
+const Rx = require('rxjs');
+const {filter, scan, map, pluck, distinctUntilChanged} = require('rxjs/operators');
+const {highlight} = require('cli-highlight');
+
 const BlockExplorer = require('./blockExplorer');
 const MinerListener = require('./minerListener');
 const MinerProcess = require('./minerProcess');
-const Rx = require('rxjs');
-const {filter, scan, map, pluck, distinctUntilChanged} = require('rxjs/operators');
-const keyListener = require('./keyListener');
+const KeyListener = require('./keyListener');
 const config = require('./config');
 const {writeInfo, writeWarning, writeError} = require('./utils');
 const state = require('./state');
-
-const minerProcess = new MinerProcess(config.MinerExe);
-const blockExplorer = new BlockExplorer(config.ExplorerApiUrl);
-const miner = new MinerListener(config.MinerWebsocketUrl);
-
-const $keys = keyListener.listen();
 
 const $fakeBlocks = Rx.Observable.interval(1000)
 	.scan((acc, curr) => acc -= 500, 500000)
 	.do(state.updateBlockFn('fake'));
 
-async function exit() {
-	writeInfo("Exiting Watchdog...", "[BYE]");
-	await minerProcess.stop();
-	process.exit(0);
+
+class Watchdog {
+
+	constructor(){
+		
+		this.__initialize = this.__initialize.bind(this);
+		this.__listenBlocks = this.__listenBlocks.bind(this);
+		this.__exit = this.__exit.bind(this);
+		this.__restart = this.__restart.bind(this);
+		
+		
+		this.subscriptionForBlocks = null;
+		this.minerProcess = new MinerProcess(config.MinerExe);
+		this.blockExplorer = new BlockExplorer(config.ExplorerApiUrl);
+		this.miner = new MinerListener(config.MinerWebsocketUrl);
+		
+		function printState() {
+			const json = highlight(JSON.stringify(state.get(), null, 4), {language:'json'});
+			writeInfo(`\n\n${json}\n`, "[STATE]");
+		}
+		
+		const keyListener = new KeyListener();
+		this.$keys = keyListener.listen();
+		this.$keys.filter(({name}) => name === 's').subscribe(printState);
+		
+		this.$exitEvent = this.$keys.filter(({name, sequence}) => name === 'escape' || sequence === '\u0003');
+		this.$exitEvent.subscribe(this.__exit);
+	}
+	
+	async __exit() {
+		writeInfo("Exiting Watchdog...", "[BYE]");
+		await this.minerProcess.stop();
+		process.exit(0);
+	}
+	
+	async __initialize() {
+		
+		writeInfo("Initializing watchdog...");
+		const isRunning = await this.minerProcess.isRunning();
+		if (!isRunning) await this.minerProcess.start();
+		
+	}
+	
+	async __restart() {
+		writeInfo("Restarting...");
+		this.subscriptionForBlocks.unsubscribe();
+		await this.minerProcess.stop(false);
+		await this.minerProcess.start();
+		setTimeout(this.__listenBlocks, 2000);
+	}
+	
+	__listenBlocks() {
+		
+		writeInfo("Start listening blocks");
+		
+		const $explorerBlockHeights = this.blockExplorer.lastBlocks().pluck('height').do(state.updateBlockFn('explorer'));
+		const $minerBlockHeights = this.miner.blockheights().do(state.updateBlockFn('miner'));
+		
+		const log = e => writeInfo(JSON.stringify(e, null, 4), '[EVENT]');
+		const split = map(e => ({miner: e[0], explorer: e[1]}));
+		const distinctHeightsOnly = distinctUntilChanged((p, q) => p.miner === q.miner && p.explorer === q.explorer);
+		const isExplorerBeforeMiner = filter(h => h.explorer > h.miner);
+		const minerBehindExplorerOnly = Rx.pipe(
+			split,
+			distinctHeightsOnly,
+			isExplorerBeforeMiner,
+		);
+		
+		const $needRestartMinerEvent = $fakeBlocks
+			.combineLatest($explorerBlockHeights)
+			.let(minerBehindExplorerOnly)
+			.takeUntil(this.$exitEvent);
+		
+		this.subscriptionForBlocks = $needRestartMinerEvent.do(log).subscribe(this.__restart);
+		
+	}
+	
+	async run() {
+		await this.__initialize();
+		this.__listenBlocks();
+	}
 }
 
-function printState() {
-	writeInfo(`\n\n${JSON.stringify(state.get(), null, 4)}\n`, "[STATE]");
-}
+const watchdog = new Watchdog();
+watchdog.run();
 
-const $exitEvent = $keys.filter(({name, sequence}) => name === 'escape' || sequence === '\u0003');
-$exitEvent.subscribe(exit);
-
-$keys.filter(({name}) => name === 's')
-	.subscribe(printState);
-
-
-async function initialize() {
-	
-	writeInfo("Initializing watchdog...");
-	const isRunning = await minerProcess.isRunning();
-	if (!isRunning) await minerProcess.start();
-	
-}
-
-
-let subscription = null;
-
-async function restart(){
-	writeInfo("Restarting...");
-	subscription.unsubscribe();
-	await minerProcess.stop(false);
-	await minerProcess.start();
-	setTimeout(listen, 2000);
-}
-
-function listen(){
-	
-	writeInfo("Start listening blocks");
-	
-	const $explorerBlockHeights = blockExplorer.lastBlocks().pluck('height').do(state.updateBlockFn('explorer'));
-	const $minerBlockHeights = miner.blockheights().do(state.updateBlockFn('miner'));
-	
-	const log = e => writeInfo(JSON.stringify(e, null, 4), '[EVENT]');
-	
-	const split = map(e => ({miner: e[0], explorer: e[1]}));
-	const distinctHeightsOnly = distinctUntilChanged((p, q) => p.miner === q.miner && p.explorer === q.explorer);
-	const isExplorerBeforeMiner = filter(h => h.explorer > h.miner);
-	const minerBehindExplorer = Rx.pipe(
-		split,
-		distinctHeightsOnly,
-		isExplorerBeforeMiner,
-	);
-	
-	const $needRestartMinerEvent = $fakeBlocks
-		.combineLatest($explorerBlockHeights)
-		.let(minerBehindExplorer)
-		.takeUntil($exitEvent);
-	
-	subscription = $needRestartMinerEvent.do(log).subscribe( restart );
-	
-}
-
-async function run(){
-	await initialize();
-	listen();
-}
-
-run();
