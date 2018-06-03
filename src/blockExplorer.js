8931333fe103d49c5a42a96e3b19a5ae29995fcb
ohager <oliver.hager@dextra-sw.com>
2018-06-03 14:16:03 -0300

    Fixed function context in blockExplorer

--- src/blockExplorer.js
+++ src/blockExplorer.js
@@ -4,14 +4,17 @@ const {writeError} = require('./utils');
 
 class BlockExplorer {
 	constructor(explorerBaseUrl, pollInterval) {
+		this.__retryStrategy = this.__retryStrategy.bind(this);
+		
 		this.explorerBaseUrl = explorerBaseUrl;
 		this.pollInterval = pollInterval * 1000;
 	}
 	
 	__retryStrategy($errors) {
+		const interval = 5 * this.pollInterval;
 		return $errors.do((err) => {
 			writeError(`Explorer has a problem:\n\t${err}`);
-		}).delay(5 * this.pollInterval)
+		}).delay(5 * interval)
 	}
 	
 	lastBlocks() {
